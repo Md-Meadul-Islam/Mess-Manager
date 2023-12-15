0766b3725973df4260ea8d696f1757c7446009b9
Md-Meadul-Islam <mdmeadulislam@gmail.com>
2023-12-15 14:39:48 +0600

    I have changed footer.php

--- resources/views/manager_mate_layouts/partials/footer.blade.php
+++ resources/views/manager_mate_layouts/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>Mess Manager</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Mess Manager Application-{{now()->format('Y')}}</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-      Designed by <a href="#"><strong>AI Pollen</strong></a>
+      Designed by <a href="#"><strong>Blooms-AI</strong></a>
     </div>
   </footer>
