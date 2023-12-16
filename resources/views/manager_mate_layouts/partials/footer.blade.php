f053befaedfa2ebfccca7836f0bdea3b722ceda1
Md-Meadul-Islam <mdmeadulislam@gmail.com>
2023-12-16 12:11:05 +0600

    footer Updated

--- resources/views/manager_mate_layouts/partials/footer.blade.php
+++ resources/views/manager_mate_layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>Mess Manager Application-{{now()->format('Y')}}</span></strong>. All Rights Reserved
+      &copy; Copyright-{{now()->format('Y')}} <strong><span>Mess Manager Application</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
       Designed by <a href="#"><strong>Blooms-AI</strong></a>
